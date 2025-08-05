ad730ca2d6f40381343d2527dbf8a1e8840d6c71
Diego Reis <diegodosreis90@gmail.com>
2025-08-05 01:05:17 -0300

    update: adicionando rotas da API de usuários

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -52,3 +52,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['api/users']['GET'] = 'api/users/index';
+$route['api/users']['POST'] = 'api/users/create';
+$route['api/users/(:num)']['GET'] = 'api/users/show/$1';
+$route['api/users/(:num)']['PUT'] = 'api/users/update/$1';
+$route['api/users/(:num)']['DELETE'] = 'api/users/delete/$1';
